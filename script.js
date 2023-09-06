9558815b414c5ac63cb8350fbe6cb160caacab18
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2023-09-05 19:57:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+document.querySelector(".ccv-input").onmouseenter = () => {
+  document.querySelector(".atm-card").style.transform =
+    "perspective(1000px) rotateY(-180deg))";
+  document.querySelector(".back").style.transform =
+    "perspective(1000px) rotateY(0deg)";
+};
+document.querySelector(".ccv-input").onmouseleave = () => {
+  document.querySelector(".atm-card").style.transform =
+    "perspective(1000px) rotateY(0deg))";
+  document.querySelector(".back").style.transform =
+    "perspective(1000px) rotateY(180deg)";
+};
+
+//For Card Number//
+document.getElementById("first").oninput = () => {
+  document.getElementById("one").innerText =
+    document.getElementById("first").value;
+  first.value = first.value.replace(/[^0-9]/g, "");
+};
+
+document.getElementById("second").oninput = () => {
+  document.getElementById("two").innerText =
+    document.getElementById("second").value;
+  second.value = second.value.replace(/[^0-9]/g, "");
+};
+
+document.getElementById("third").oninput = () => {
+  document.getElementById("three").innerText =
+    document.getElementById("third").value;
+  third.value = third.value.replace(/[^0-9]/g, "");
+};
+
+document.getElementById("fourth").oninput = () => {
+  document.getElementById("four").innerText =
+    document.getElementById("fourth").value;
+  fourth.value = fourth.value.replace(/[^0-9]/g, "");
+};
+
+//For CCV box//
+document.getElementById("ccvBox").oninput = () => {
+  document.querySelector(".ccv-box").innerText =
+    document.getElementById("ccvBox").value;
+  ccvBox.value = ccvBox.value.replace(/[^0-9]/g, "");
+};
+
+//For Card Holder//
+document.querySelector(".card-holder-input").oninput = () => {
+  document.querySelector(".card-holder-name").innerText =
+    document.querySelector(".card-holder-input").value;
+};
+
+//For Expiration date//
+document.querySelector(".month-input").oninput = () => {
+  document.querySelector(".exp-month").innerText =
+    document.querySelector(".month-input").value;
+};
+
+document.querySelector(".year-input").oninput = () => {
+  document.querySelector(".exp-year").innerText =
+    document.querySelector(".year-input").value;
+};
+
+//To change Logo//
+const first = document.getElementById("first");
+const logo = document.getElementById("logo");
+
+first.addEventListener("input", function () {
+  const inputValue = first.value;
+  changeCardLogo(inputValue);
+});
+
+function changeCardLogo(inputValue) {
+  if (inputValue.startsWith("5") || inputValue.startsWith("2")) {
+    logo.src = "mastercard.png";
+  } else if (inputValue.startsWith("4")) {
+    logo.src = "visa.png";
+  } else if (inputValue.startsWith("3")) {
+    logo.src = "amex.png";
+  }
+}
+
+//Submission//
+var form = document.getElementById("form");
+var message = document.getElementById("message");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  //message.textContent = "Your payment was successful";
+  alert("Your payment was successful");
+});
